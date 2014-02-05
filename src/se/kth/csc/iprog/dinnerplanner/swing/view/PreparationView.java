6f9248fd2fc7cb9a89ece8458368ab685ce9999c
hendrik <hendrikheuer@gmail.com>
2014-02-05 16:33:41 +0100

    added DishView, PreparationView, IngredientView, MainContentView, MainSidebarView

---
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/PreparationView.java
@@ -0,0 +1,33 @@
+package se.kth.csc.iprog.dinnerplanner.swing.view;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+
+
+public class PreparationView extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+	
+	// The components of our view
+	JLabel label = new JLabel();
+	JButton plusButton = new JButton("World");
+	JButton minusButton = new JButton("Wayne's");
+	
+	public PreparationView(){
+
+		label.setText("Hello");
+		
+		// Add label to the view
+		this.add(label);
+		
+		this.add(plusButton);
+		this.add(minusButton);
+		
+		
+		// Setup the rest of the view layout
+	}
+	
+}
