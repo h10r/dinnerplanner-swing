e40de1614398b483f1876ca15ddcf45689b2d5e6
hendrik <hendrikheuer@gmail.com>
2014-02-05 16:14:11 +0100

    removed unused line

--- src/se/kth/csc/iprog/dinnerplanner/model/DinnerModel.java
+++ src/se/kth/csc/iprog/dinnerplanner/model/DinnerModel.java
@@ -110,7 +110,6 @@ public class DinnerModel implements IDinnerModel {
 	 * Returns the dish that is on the menu for selected type (1 = starter, 2 = main, 3 = desert).
 	 */
 	public Dish getSelectedDish(int type) {
-		Set<Dish> result = new HashSet<Dish>();
 		for(Dish d : dishes){
 			if(d.getType() == type){
 				return d;
