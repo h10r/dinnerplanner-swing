61fef26743d2d99672de06036a69541c2ba07ebf
hendrik <hendrikheuer@gmail.com>
2014-02-06 13:00:15 +0100

    updated MainContentView and MainSidebarView

--- src/se/kth/csc/iprog/dinnerplanner/swing/view/MainContentView.java
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/MainContentView.java
@@ -5,26 +5,109 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 
 public class MainContentView extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-	
-	// The components of our view
-	JLabel label = new JLabel();
-	JButton plusButton = new JButton("World");
-	JButton minusButton = new JButton("Wayne's");
+	private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+	private final JPanel desertPanel = new JPanel();
+	private final JTextField txtDesertSearchBar = new JTextField();
+	private final JPanel desertContentPanel = new JPanel();
+	private final JPanel mainPanel = new JPanel();
+	private final JTextField txtMainSearchBar = new JTextField();
+	private final JPanel mainContentPanel = new JPanel();
+	private final JPanel starterPanel = new JPanel();
+	private final JTextField txtStarterSearchBar = new JTextField();
+	private final JPanel starterContentPanel = new JPanel();
 	
 	public MainContentView(){
-
-		label.setText("MainContent");
+		setLayout(new BorderLayout(0, 0));
 		
-		// Add label to the view
-		this.add(label);
+		add(tabbedPane, BorderLayout.WEST);
+		GridBagLayout gbl_starterPanel = new GridBagLayout();
+		gbl_starterPanel.columnWidths = new int[]{147, 134, 0};
+		gbl_starterPanel.rowHeights = new int[]{28, 0, 0};
+		gbl_starterPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_starterPanel.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		tabbedPane.addTab("Starter", null, starterPanel, null);
+		starterPanel.setLayout(gbl_starterPanel);
 		
-		this.add(plusButton);
-		this.add(minusButton);
+		GridBagConstraints gbc_txtStarterSearchBar = new GridBagConstraints();
+		gbc_txtStarterSearchBar.anchor = GridBagConstraints.NORTHWEST;
+		gbc_txtStarterSearchBar.gridwidth = 2;
+		gbc_txtStarterSearchBar.insets = new Insets(0, 0, 5, 0);
+		gbc_txtStarterSearchBar.gridx = 0;
+		gbc_txtStarterSearchBar.gridy = 0;
+		txtStarterSearchBar.setText("Search dish...");
+		txtStarterSearchBar.setColumns(10);
+		starterPanel.add(txtStarterSearchBar, gbc_txtStarterSearchBar);
+		
+		GridBagConstraints gbc_starterContentPanel = new GridBagConstraints();
+		gbc_starterContentPanel.gridwidth = 2;
+		gbc_starterContentPanel.fill = GridBagConstraints.BOTH;
+		gbc_starterContentPanel.insets = new Insets(0, 0, 0, 5);
+		gbc_starterContentPanel.gridx = 0;
+		gbc_starterContentPanel.gridy = 1;
+		starterPanel.add(starterContentPanel, gbc_starterContentPanel);
+		
+		tabbedPane.addTab("Main", null, mainPanel, null);
+		GridBagLayout gbl_mainPanel = new GridBagLayout();
+		gbl_mainPanel.columnWidths = new int[]{147, 134, 0};
+		gbl_mainPanel.rowHeights = new int[]{28, 0, 0};
+		gbl_mainPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_mainPanel.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		mainPanel.setLayout(gbl_mainPanel);
+		
+		GridBagConstraints gbc_txtMainSearchBar = new GridBagConstraints();
+		gbc_txtMainSearchBar.anchor = GridBagConstraints.NORTHWEST;
+		gbc_txtMainSearchBar.gridwidth = 2;
+		gbc_txtMainSearchBar.insets = new Insets(0, 0, 5, 0);
+		gbc_txtMainSearchBar.gridx = 0;
+		gbc_txtMainSearchBar.gridy = 0;
+		txtMainSearchBar.setText("Search dish...");
+		txtMainSearchBar.setColumns(10);
+		mainPanel.add(txtMainSearchBar, gbc_txtMainSearchBar);
+		
+		GridBagConstraints gbc_mainContentPanel = new GridBagConstraints();
+		gbc_mainContentPanel.gridwidth = 2;
+		gbc_mainContentPanel.fill = GridBagConstraints.BOTH;
+		gbc_mainContentPanel.insets = new Insets(0, 0, 0, 5);
+		gbc_mainContentPanel.gridx = 0;
+		gbc_mainContentPanel.gridy = 1;
+		mainPanel.add(mainContentPanel, gbc_mainContentPanel);
+		
+		tabbedPane.addTab("Desert", null, desertPanel, null);
+		GridBagLayout gbl_desertPanel = new GridBagLayout();
+		gbl_desertPanel.columnWidths = new int[]{147, 134, 0};
+		gbl_desertPanel.rowHeights = new int[]{28, 0, 0};
+		gbl_desertPanel.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gbl_desertPanel.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		desertPanel.setLayout(gbl_desertPanel);
+		txtDesertSearchBar.setText("Search dish...");
+		txtDesertSearchBar.setColumns(10);
+		
+		GridBagConstraints gbc_txtDesertSearchBar = new GridBagConstraints();
+		gbc_txtDesertSearchBar.insets = new Insets(0, 0, 5, 0);
+		gbc_txtDesertSearchBar.gridwidth = 2;
+		gbc_txtDesertSearchBar.anchor = GridBagConstraints.NORTHWEST;
+		gbc_txtDesertSearchBar.gridx = 0;
+		gbc_txtDesertSearchBar.gridy = 0;
+		desertPanel.add(txtDesertSearchBar, gbc_txtDesertSearchBar);
+		
+		GridBagConstraints gbc_desertContentPanel = new GridBagConstraints();
+		gbc_desertContentPanel.gridwidth = 2;
+		gbc_desertContentPanel.insets = new Insets(0, 0, 0, 5);
+		gbc_desertContentPanel.fill = GridBagConstraints.BOTH;
+		gbc_desertContentPanel.gridx = 0;
+		gbc_desertContentPanel.gridy = 1;
+		desertPanel.add(desertContentPanel, gbc_desertContentPanel);
 		
 		
 		// Setup the rest of the view layout
--- src/se/kth/csc/iprog/dinnerplanner/swing/view/MainSidebarView.java
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/MainSidebarView.java
@@ -12,6 +12,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.DefaultComboBoxModel;
 
 public class MainSidebarView extends JPanel {
 	
@@ -21,49 +27,78 @@ public class MainSidebarView extends JPanel {
 	
 	public MainSidebarView(){
 		
-		this.setLayout( new GridLayout(0,2) );
+		String[] numberOfPeople = { "1","2","3","4","5","6","7","8","9","10","11","12" };
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{84, 130, 0, 135, 0};
+		gridBagLayout.rowHeights = new int[]{27, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 		
 		JLabel lblNumberOfPeople = new JLabel("Number of People");
+		GridBagConstraints gbc_lblNumberOfPeople = new GridBagConstraints();
+		gbc_lblNumberOfPeople.anchor = GridBagConstraints.WEST;
+		gbc_lblNumberOfPeople.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNumberOfPeople.gridx = 1;
+		gbc_lblNumberOfPeople.gridy = 0;
+		add(lblNumberOfPeople, gbc_lblNumberOfPeople);
 		
-		JLabel lblTotalCosts = new JLabel("Total cost:");
-		JLabel lblTotalCostsValue = new JLabel("$ 0.00");
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}));
+		comboBox.setSelectedIndex(3);
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.anchor = GridBagConstraints.NORTHWEST;
+		gbc_comboBox.gridx = 2;
+		gbc_comboBox.gridy = 0;
+		add(comboBox, gbc_comboBox);
 		
-		JLabel lblTitle = new JLabel("Dinner Menu");
+		JLabel lblTotalCostDescription = new JLabel("Total costs");
+		lblTotalCostDescription.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_lblTotalCostDescription = new GridBagConstraints();
+		gbc_lblTotalCostDescription.anchor = GridBagConstraints.WEST;
+		gbc_lblTotalCostDescription.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTotalCostDescription.gridx = 1;
+		gbc_lblTotalCostDescription.gridy = 1;
+		add(lblTotalCostDescription, gbc_lblTotalCostDescription);
 		
-		JLabel lblDragArea = new JLabel("Drag your dish to the menu");
+		JLabel lblTotalCost = new JLabel("SEK 130");
+		GridBagConstraints gbc_lblTotalCost = new GridBagConstraints();
+		gbc_lblTotalCost.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTotalCost.gridx = 2;
+		gbc_lblTotalCost.gridy = 1;
+		add(lblTotalCost, gbc_lblTotalCost);
+		
+		JLabel lblDinerMenuLabel = new JLabel("Dinner Menu");
+		GridBagConstraints gbc_lblDinerMenuLabel = new GridBagConstraints();
+		gbc_lblDinerMenuLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDinerMenuLabel.gridwidth = 2;
+		gbc_lblDinerMenuLabel.gridx = 1;
+		gbc_lblDinerMenuLabel.gridy = 2;
+		add(lblDinerMenuLabel, gbc_lblDinerMenuLabel);
+		
+		JLabel lblDishDragNDrop = new JLabel("Drag your dish to the menu");
+		GridBagConstraints gbc_lblDishDragNDrop = new GridBagConstraints();
+		gbc_lblDishDragNDrop.gridheight = 4;
+		gbc_lblDishDragNDrop.gridwidth = 2;
+		gbc_lblDishDragNDrop.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDishDragNDrop.gridx = 1;
+		gbc_lblDishDragNDrop.gridy = 3;
+		add(lblDishDragNDrop, gbc_lblDishDragNDrop);
 		
 		JButton btnPreparation = new JButton("Preparation");
-		/*
-		btnPreparation.addActionListener( new ActionListener() {			 
-            public void actionPerformed(ActionEvent e)
-            {
-            	frame1.getContentPane().add( new PreparationView() );
-            	
-            	System.out.println("You clicked the button");
-            }
-        });
-		*/
-   
+		GridBagConstraints gbc_btnPreparation = new GridBagConstraints();
+		gbc_btnPreparation.insets = new Insets(0, 0, 0, 5);
+		gbc_btnPreparation.gridx = 1;
+		gbc_btnPreparation.gridy = 7;
+		add(btnPreparation, gbc_btnPreparation);
+		
 		JButton btnIngredients = new JButton("Ingredients");
-		
-		String[] numberOfPeople = { "1","2","3","4","5","6","7","8","9","10","11","12" };
-		JComboBox comboNumberOfPeople = new JComboBox(numberOfPeople);
-		
-		this.add(lblNumberOfPeople);
-		this.add(lblNumberOfPeople);
-		
-		comboNumberOfPeople.setSelectedIndex(3);
-		this.add(comboNumberOfPeople);
-		
-		this.add(lblTotalCosts);
-		this.add(lblTotalCostsValue);
-		
-		this.add(lblTitle);
-		
-		this.add(lblDragArea);
-		
-		this.add(btnPreparation);
-		this.add(btnIngredients);
+		GridBagConstraints gbc_btnIngredients = new GridBagConstraints();
+		gbc_btnIngredients.insets = new Insets(0, 0, 0, 5);
+		gbc_btnIngredients.gridx = 2;
+		gbc_btnIngredients.gridy = 7;
+		add(btnIngredients, gbc_btnIngredients);
 	}
 	
 }
