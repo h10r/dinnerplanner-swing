4586f796a48585f9fcfe2199192eb0a0b4fc357d
hendrik <hendrikheuer@gmail.com>
2014-02-07 16:20:47 +0100

    added MainContentPanelView

---
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/MainContentPanelView.java
@@ -0,0 +1,33 @@
+package se.kth.csc.iprog.dinnerplanner.swing.view;
+
+import java.awt.BorderLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
+
+import java.awt.Font;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import javax.swing.JTable;
+import java.awt.Dimension;
+import javax.swing.JScrollPane;
+import java.awt.Insets;
+
+
+public class MainContentPanelView extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+
+	public MainContentPanelView(DinnerModel modelInstance){
+	
+	}	
+}
